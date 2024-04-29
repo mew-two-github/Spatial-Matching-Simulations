931b2d9eaa7095246cee95d4b5a24b1522769501
vishal <subhavishal@yahoo.co.in>
2024-04-29 15:24:13 -0400

    initial commit - working maxweight and batching with spatial and random weights

---
+++ spatial.py
@@ -0,0 +1,124 @@
+import numpy as np
+from scipy.stats import rv_discrete
+from matplotlib import pyplot as plt
+import cvxpy as cp
+import utils
+import pickle
+# from tempfile import TemporaryFile
+
+def run_max_weight( time_steps, N, pmf_cust, pmf_serv, W, alpha_values, gurobi_flag = 1 ):
+    
+    arrival_ids = np.arange(0,N,1)
+    cust_process = rv_discrete(name='cust_arr', values=(arrival_ids, pmf_cust))
+    serv_process = rv_discrete(name='serv_arr', values=(arrival_ids, pmf_serv))
+    
+    Q_paths = []
+    C_paths = []
+    for alpha in alpha_values:
+        if gurobi_flag:
+            res = utils.max_weight_gurobi( time_steps, N, cust_process, serv_process, W, alpha )
+        else:
+            res = utils.max_weight( time_steps, N, cust_process, serv_process, W, alpha )
+        Q_paths.append(res["QP"])
+        C_paths.append(res["CP"])
+    return({"Q_paths": Q_paths,"C_paths":C_paths})
+
+def run_batching( time_steps, N, pmf_cust, pmf_serv, W, Tbatches, gurobi_flag = 1 ):
+    
+    arrival_ids = np.arange(0,N,1)
+    cust_process = rv_discrete(name='cust_arr', values=(arrival_ids, pmf_cust))
+    serv_process = rv_discrete(name='serv_arr', values=(arrival_ids, pmf_serv))
+    
+    Q_paths = []
+    C_paths = []
+    for T in Tbatches:
+        if gurobi_flag:
+            res = utils.batching_gurobi( time_steps, N, cust_process, serv_process, W, T )
+        else:
+            res = utils.batching( time_steps, N, cust_process, serv_process, W, T )
+        Q_paths.append(res["QP"])
+        C_paths.append(res["CP"])
+    return({"Q_paths": Q_paths,"C_paths":C_paths})
+
+def plot_from_sim_paths(file_name):
+    with open(file_name,'rb') as f:
+        M_res,B_res, N, W, pmf_cust, pmf_serv = pickle.load(f)
+    print(M_res)
+    Q_paths_M = M_res["Q_paths"]
+    C_paths_M = M_res["C_paths"]
+    Q_paths_B = B_res["Q_paths"]
+    C_paths_B = B_res["C_paths"]
+    (average_cost_M, average_queue_M) = utils.average_paths(C_paths_M,Q_paths_M)
+    (average_cost_B, average_queue_B) = utils.average_paths(C_paths_B,Q_paths_B)
+    [Ex,fluid_sol] = run_extremes(N,W,pmf_cust,pmf_serv)
+    utils.qc_plot(Ex,fluid_sol,average_queue_B,average_cost_B,average_queue_M,average_cost_M)
+
+def generate_weights(N_grid, is_spatial = 0):
+    Weights = np.zeros(shape=(N_grid,N_grid))
+    if is_spatial:
+        X = np.arange(0.5,N_grid,1)
+        Y = np.arange(0.5,N_grid,1)
+        cell_locs = []
+        for i in range(N_grid):
+            for j in range(N_grid):
+                cell_locs.append([X[i],Y[j]])
+        N_cells = N_grid*N_grid
+        Weights = np.zeros(shape=(N_cells,N_cells))
+
+        for i in range(N_cells):
+            for j in range(N_cells):
+                Weights[i,j] = np.sqrt((cell_locs[i][0] - cell_locs[j][0])**2 + (cell_locs[i][1] - cell_locs[j][1])**2)
+        print("Spatial",Weights)
+    else:
+        rand_weights = np.random.rand(N_grid, N_grid)
+        N_cells = N_grid*N_grid
+        for counter in range(N_cells):
+            for i in range(N_grid):
+                for j in range(N_grid):
+                    for k in range(N_grid):
+                        rand_weights[i,j] = min(rand_weights[i,j],rand_weights[i,k] + rand_weights[k,j] )
+        print("Random",rand_weights)
+        Weights = rand_weights
+    return(Weights)
+
+# Problem setup
+N = 4
+is_spatial = 1
+W = generate_weights(N,is_spatial)
+time_steps_M = 100000
+time_steps_B = 100000
+
+Tbatches = np.asarray( [1,2, 3, 4, 5, 8, 10, 12, 15, 18, 20, 30, 40, 50, 60, 100  ] )
+# alpha_values = np.asarray( [0, 0.2, 0.25, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.8,0.9,0.95] )
+alpha_values = np.asarray( [0, 0.2, 0.25, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.675, 0.7, 0.725, 0.75, 0.8, 0.825, 0.85, 0.875,  0.9, 0.925, 0.95] )
+
+if is_spatial == 1:
+    N = N*N
+
+pmf_cust = np.random.rand(N)
+pmf_cust = pmf_cust/(np.sum(pmf_cust))
+
+pmf_serv = np.random.rand(N)
+pmf_serv = pmf_serv/(np.sum(pmf_serv))
+# pmf_cust = [0.19,0.31,0.42,0.08]
+# pmf_serv = [0.09,0.23,0.41,0.27]
+print(pmf_cust,pmf_serv)
+
+M_res = run_max_weight( time_steps_M, N, pmf_cust, pmf_serv, W, alpha_values,0 )
+Q_paths_M = M_res["Q_paths"]
+C_paths_M = M_res["C_paths"]
+
+
+B_res = run_batching( time_steps_B, N, pmf_cust, pmf_serv, W, Tbatches,0 )
+Q_paths_B = B_res["Q_paths"]
+C_paths_B = B_res["C_paths"]
+(average_cost_M, average_queue_M) = utils.average_paths(C_paths_M,Q_paths_M)
+(average_cost_B, average_queue_B) = utils.average_paths(C_paths_B,Q_paths_B)
+
+with open('/storage/home/hcoda1/7/vsivaraman3/p-smaguluri3-0/spatial-matching-sim-vishal/sim_paths_spatial.pkl', 'wb') as f:  
+# with open('./sim_paths.pkl', 'wb') as f: 
+    pickle.dump([M_res,B_res, N, W, pmf_cust, pmf_serv], f)
+
+print("Run completed successfully")
+
+
---
+++ utils.py
@@ -0,0 +1,297 @@
+import numpy as np
+from scipy.stats import rv_discrete
+from matplotlib import pyplot as plt
+import cvxpy as cp
+import gurobipy as gp
+from gurobipy import GRB
+
+def fluid_solution(N, W, pmf_cust, pmf_serv):
+    x = cp.Variable((N,N)) # fraction of matchings from i to j
+    print(W)
+    constraints = []                          
+    for i in range(N):
+        for j in range(N):
+            constraints.append(x[i,j] >= 0)
+            constraints.append(x[i,j] <= 1)
+    # inequality to avoid round off, and violating equality is anyway penalised by objective
+    for i in range(N):
+        constraints.append(cp.sum(x[i,:]) == pmf_cust[i] ) 
+    for j in range(N):
+        constraints.append(cp.sum(x[:,j]) == pmf_serv[j] )                      
+
+    objective = cp.Minimize( cp.sum( cp.multiply( x , W ) ) )
+    problem = cp.Problem(objective, constraints)
+    problem.solve()  
+    print(problem)
+    return({"X": x.value, "Cost": problem.value })
+
+
+def fluid_solution_gurobi(N, W, pmf_cust, pmf_serv):
+
+    model = gp.Model("Fluid Model")
+
+    x = model.addMVar((N,N),lb=0,ub=1.0,vtype=GRB.CONTINUOUS, name="x")
+    
+    # Set objective
+    obj = gp.quicksum(x[i, j] * W[i,j]
+                            for i in range(N)
+                            for j in range(N))
+    model.setObjective( obj, GRB.MINIMIZE )
+    
+    # for i in range(N):
+    #     for j in range(N):
+    #         model.addConstr( x[i,j] >= 0 )
+    #         model.addConstr( x[i,j] <= 1 )
+    
+    for i in range(N):
+        model.addConstr( gp.quicksum(x[i,:]) == pmf_cust[i] )
+    for j in range(N):
+        model.addConstr( gp.quicksum(x[:,j]) == pmf_serv[j] )
+    # Optimize model
+    model.optimize()
+    # Print solution
+    if model.status == GRB.OPTIMAL:
+        print('Objective Value: %.2f' % model.objVal)
+        print('x: ', x.x)
+    else:
+        print('Optimization failed. Status:', model.status)
+    return({"Cost": model.objVal, "X_matrix": x.x })
+
+def average_paths(Cost_paths, Queue_paths, fract = 0, plot = 0):
+    Queue_paths = np.transpose( np.asarray(Queue_paths) )
+    Cost_paths = np.transpose( np.asarray(Cost_paths) )
+    time_steps = len(Queue_paths[:,0])
+    average_queue_lengths = np.average(Queue_paths[int(time_steps*fract):,:], axis = 0)
+    average_cost = np.average(Cost_paths[int(time_steps*fract):,:], axis=0)
+    if plot:
+        plt.plot(average_queue_lengths,average_cost,'x')
+        plt.show()
+    return(average_cost,average_queue_lengths)
+
+def max_weight( time_steps, N, cust_process, serv_process, W, alpha ):
+
+    cust_arrivals = cust_process.rvs( size = time_steps )
+    serv_arrivals = serv_process.rvs( size = time_steps )
+
+    q = np.zeros(N)
+    q[cust_arrivals[0]] = 1
+    qtilde = np.zeros(N)
+    qtilde[serv_arrivals[0]] = 1
+
+    cost_path = [0]
+    total_queue = []
+    total_queue.append(np.sum(q))
+
+    for k in range(1,time_steps):
+        # Maxweight Matching
+        M = cp.Variable((N,N),integer=True)
+        constraints = []
+        for i in range(N):
+            constraints.append( cp.sum(M[i,:]) <= q[i] )
+            constraints.append( cp.sum(M[:,i]) <= qtilde[i] )
+        constraints.append( M[:,:]>=0 )
+
+        objective = cp.Maximize( cp.sum( (1-alpha)*( cp.multiply( q, cp.sum(M, axis=1) ) + cp.multiply( qtilde, cp.sum(M, axis=0) ) ) )
+                                - cp.sum( alpha*cp.multiply( W,M  ) ) )
+        problem  = cp.Problem(objective, constraints)         
+        problem.solve(solver="GLPK_MI")
+        # print("q=",q)
+        # print("qtilde=",qtilde)
+        # print(problem)
+        
+
+        # Check arrivals
+        a = np.zeros(N)
+        atilde = np.zeros(N)
+
+        a[cust_arrivals[k]] = 1
+        atilde[serv_arrivals[k]] = 1
+        # State Update
+        Mstar = M.value
+        # print("M=", Mstar)
+        matching_cost = np.sum(np.multiply(Mstar,W)) 
+        q = q + a - np.sum(Mstar, 1)
+        qtilde = qtilde + atilde - np.sum(Mstar,0)
+        
+        total_queue.append(np.sum(q))
+        cost_path.append(matching_cost)
+    
+    return({"QP": total_queue, "CP": cost_path})
+
+def batching( time_steps, N, cust_process, serv_process, W, T ):
+
+    # Generate arrivals
+    cust_arrivals = cust_process.rvs( size = time_steps )
+    serv_arrivals = serv_process.rvs( size = time_steps )
+
+    q = np.zeros(N)
+    q[cust_arrivals[0]] = 1
+    qtilde = np.zeros(N)
+    qtilde[serv_arrivals[0]] = 1
+    
+    running_cost = 0
+    cost_path = [0]
+    total_queue = [] 
+    total_queue.append(np.sum(q))
+
+    for k in range(1,time_steps):
+        # Check arrivals
+        a = np.zeros(N)
+        atilde = np.zeros(N)
+
+        a[cust_arrivals[k]] = 1
+        atilde[serv_arrivals[k]] = 1
+        # State Update
+        Mstar = np.zeros(shape=(N,N))
+        
+        # Matching is done every T time steps 
+        if(k%T==0):
+            constraints = []
+            M = cp.Variable((N,N),integer=True)
+            constraints = []
+            for i in range(N):
+                constraints.append( cp.sum(M[i,:]) == q[i] )
+                constraints.append( cp.sum(M[:,i]) == qtilde[i] )
+            constraints.append( M[:,:]>=0 )
+
+            objective = cp.Minimize( cp.sum( cp.multiply( W, M ) ) )
+            problem  = cp.Problem(objective, constraints)         
+            problem.solve(solver="GLPK_MI")
+            Mstar = M.value
+
+        running_cost += np.sum(np.multiply(Mstar,W))
+        q = q + a - np.sum(Mstar, 1)
+        if q[0] < 0:
+            print(q-a+np.sum(Mstar, 1))
+            print(Mstar, k)
+            print(constraints)
+            break
+        qtilde = qtilde + atilde - np.sum(Mstar,0)
+        
+        total_queue.append(np.sum(q))
+        cost_path.append(np.sum(np.multiply(Mstar,W)))
+    return({"QP": total_queue, "CP": cost_path})
+
+
+def qc_plot( Ex, fluid_sol, avg_Q_B, avg_C_B, avg_Q_M, avg_C_M ):
+    
+    plt.figure( figsize=(10,6) ) 
+    
+    # C = 0 Case
+    
+    # C = Inf/Fluid Case
+    xlim = max( max( avg_Q_B ), max( avg_Q_M ) )
+    plt.plot([0,xlim],[fluid_sol['Cost'],fluid_sol['Cost']],linewidth = 3)
+    # Batching data
+    plt.plot(avg_Q_B,avg_C_B,'x',markersize = 10, markeredgewidth=3, markeredgecolor="darkblue")
+    # Max Weight data
+    plt.plot(avg_Q_M,avg_C_M, 'go',markersize = 8)
+    plt.plot([1],[Ex],'r*',markersize = 12, markeredgewidth=3)
+    plt.xlabel("Average queue length", weight='bold',fontsize=12)
+    plt.ylabel("Average cost", weight='bold',fontsize=12)
+    plt.title("c vs q", weight='bold',fontsize=15)
+    plt.legend(['Fluid', 'Batching', 'Max-weight', 'c = 0 sol'])
+    # plt.legend([ 'Batching', 'Max-weight', 'c = 0 sol'])
+    plt.grid(True)
+    plt.show()
+
+def batching_gurobi( time_steps, N, cust_process, serv_process, W, T ):
+
+    # Generate arrivals
+    cust_arrivals = cust_process.rvs( size = time_steps )
+    serv_arrivals = serv_process.rvs( size = time_steps )
+
+    q = np.zeros(N)
+    q[cust_arrivals[0]] = 1
+    qtilde = np.zeros(N)
+    qtilde[serv_arrivals[0]] = 1
+    
+    running_cost = 0
+    cost_path = [0]
+    total_queue = [] 
+    total_queue.append(np.sum(q))
+
+    for k in range(1,time_steps):
+        # Check arrivals
+        a = np.zeros(N)
+        atilde = np.zeros(N)
+        
+        a[cust_arrivals[k]] = 1
+        atilde[serv_arrivals[k]] = 1
+        # State Update
+        Mstar = np.zeros(shape=(N,N))
+        
+        # Matching is done every T time steps 
+        if(k%T==0):
+            model = gp.Model()
+            M = model.addMVar((N,N),lb=0, vtype=GRB.INTEGER)
+            model.setParam('OutputFlag',0)
+            # Set objective
+            obj = gp.quicksum(M[i, j] * W[i,j] for i in range(N) for j in range(N))
+            model.setObjective( obj, GRB.MINIMIZE )
+            
+            for i in range(N):
+                model.addConstr( gp.quicksum(M[i,:]) >= q[i] )
+            for j in range(N):
+                model.addConstr( gp.quicksum(M[:,j]) >= qtilde[j] )
+            model.optimize()
+            
+            if model.status == GRB.OPTIMAL:
+                Mstar = M.x
+            else:
+                print('Optimization failed. Status:', model.status)
+            
+
+        running_cost += np.sum(np.multiply(Mstar,W))
+        q = q + a - np.sum(Mstar, 1)
+        qtilde = qtilde + atilde - np.sum(Mstar,0)
+        
+        total_queue.append(np.sum(q))
+        cost_path.append(np.sum(np.multiply(Mstar,W)))
+    return({"QP": total_queue, "CP": cost_path})
+
+def max_weight_gurobi( time_steps, N, cust_process, serv_process, W, alpha ):
+
+    cust_arrivals = cust_process.rvs( size = time_steps )
+    serv_arrivals = serv_process.rvs( size = time_steps )
+
+    q = np.zeros(N)
+    q[cust_arrivals[0]] = 1
+    qtilde = np.zeros(N)
+    qtilde[serv_arrivals[0]] = 1
+
+    cost_path = [0]
+    total_queue = []
+    total_queue.append(np.sum(q))
+
+    for k in range(1,time_steps):
+        model = gp.Model()
+        M = model.addMVar((N,N),lb=0, vtype = GRB.INTEGER)
+        obj = gp.quicksum( ((1-alpha)*(q[i] + qtilde[j]) - alpha*W[i,j])*M[i,j] for i in range(N) 
+                                                                for j in range(N) )
+        for i in range(N):
+            model.addConstr( gp.quicksum( M[i,j] for j in range(N) )<= q[i] )
+        for j in range(N):
+            model.addConstr( gp.quicksum( M[i,j] for i in range(N) )<= qtilde[j] )
+        model.setObjective(obj, GRB.MAXIMIZE)
+        model.setParam('OutputFlag',0)
+        model.setParam('Method',1)
+        model.optimize()
+        a = np.zeros(N)
+        atilde = np.zeros(N)
+
+        a[cust_arrivals[k]] = 1
+        atilde[serv_arrivals[k]] = 1
+
+        if model.status == GRB.OPTIMAL:
+            Matches = M.x
+            matching_cost = np.sum(np.multiply(Matches,W))
+            q = q + a - np.sum(Matches, 1)
+            qtilde = qtilde + atilde - np.sum(Matches,0)
+            total_queue.append(np.sum(q))
+            cost_path.append(matching_cost)
+        else:
+            print('Optimization failed. Status:', model.status, '\t iteration', k)
+            break
+
+    return({"QP": total_queue, "CP": cost_path})

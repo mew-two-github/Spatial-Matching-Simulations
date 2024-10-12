18609d1fefb5b9d02f6cd51648c8ad1b11f5f278
vishal <subhavishal@yahoo.co.in>
2024-10-12 15:56:51 -0400

    continous space batching simulation

--- continous_space/batching-new.py
+++ continous_space/batching-new.py
@@ -3,6 +3,7 @@ from scipy.stats import rv_discrete
 from matplotlib import pyplot as plt
 import cvxpy as cp
 import pickle
+from scipy.optimize import linear_sum_assignment
 
 def batch_single_path(time_steps, grid_size, Tbatch):
     cust_arrivals_x = np.random.uniform(low=0, high=grid_size, size=time_steps)
@@ -21,40 +22,25 @@ def batch_single_path(time_steps, grid_size, Tbatch):
     total_queue = []
     total_queue.append(len(customers))
 
-    count = 0
-
     for k in range(1,time_steps):
+
         queue_length = len(customers)
-        Mstar = np.zeros(shape=(queue_length,queue_length))
         W = np.zeros(shape=(queue_length,queue_length))
+        matching_cost = 0
+
         # Batching
         if k%Tbatch == 0:
-            N = len(customers)
-            M = cp.Variable((N,N),boolean=True)
-            constraints = []
-            for i in range(N):
-                # Match customer i with 1 server
-                constraints.append( cp.sum(M[i,:]) == 1 )
-                # Match server i with 1 customer
-                constraints.append( cp.sum(M[:,i]) == 1 )
-            constraints.append( M[:,:]>=0 )
-            
             coords1 = np.array(customers)
             coords2 = np.array(servers)
             diff = coords1[:, np.newaxis, :] - coords2[np.newaxis, :, :]
             W = np.sqrt(np.sum(diff**2, axis=-1))
-            objective = cp.Minimize( cp.sum( cp.multiply( W, M ) ) )
-            
-            problem  = cp.Problem(objective, constraints)
-            problem.solve(solver="GLPK_MI")
-            Mstar = M.value
+            row_ind, col_ind = linear_sum_assignment(W)
+            matching_cost = W[row_ind,col_ind].sum()
             customers = []
             servers = []
 
         customers.append([cust_arrivals_x[k],cust_arrivals_y[k]])
         servers.append([serv_arrivals_x[k],serv_arrivals_y[k]])
-
-        matching_cost = np.sum( np.multiply( W, Mstar ) )
 
         total_queue.append(len(customers))
         cost_path.append(matching_cost)
@@ -64,18 +50,19 @@ def batch_single_path(time_steps, grid_size, Tbatch):
 # Problem setup
 N = 2
 
-time_steps = 100
+time_steps = 100000
 
 Qpaths = []
 Cpaths = []
 
 # Tbatches = np.asarray( [3, 5, 8, 10, 12, 15, 18, 20, 30, 40, 50, 60, 100  ] )
-Tbatches = np.asarray( [1,2, 3, 4, 5, 8, 10, 12, 15, 18, 20, 30, 40, 50, 60, 100  ] )
+# Tbatches = np.asarray( [1,2, 3, 4, 5, 8, 10, 12, 15, 18, 20, 30, 40, 50, 60, 100  ] )
+Tbatches = np.arange(200,3001,200)
 for Tbatch in Tbatches:
     return_val = batch_single_path(time_steps, N, Tbatch)
     Qpaths.append(return_val["QP"])
     Cpaths.append(return_val["CP"])
 
-with open('/storage/home/hcoda1/7/vsivaraman3/p-smaguluri3-0/spatial-matching-sim-vishal/continous/batch.pkl', 'wb') as f:  
-# with open('./sim_paths_batch.pkl', 'wb') as f:
+with open('/storage/home/hcoda1/7/vsivaraman3/p-smaguluri3-0/spatial-matching-sim-vishal/continous/batch-new.pkl', 'wb') as f:  
+# with open('./batch-new.pkl', 'wb') as f:
     pickle.dump([Qpaths,Cpaths], f)
